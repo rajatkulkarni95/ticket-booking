bcded808d81c01ab670c093758076065556614c9
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-01-22 19:33:10 +0530

    Basic breakpoints and styling setup for stitches

---
+++ stitches.config.ts
@@ -0,0 +1,35 @@
+import { createStitches } from "@stitches/react";
+import {
+  violetDark,
+  indigoDark,
+  grayDark,
+  slateDark,
+  tomatoDark,
+  grassDark,
+  sageDark,
+} from "@radix-ui/colors";
+
+export const { styled, getCssText, createTheme } = createStitches({
+  media: {
+    phone: `(width < 720px)`,
+    tablet: `(720px <= width < 1024px)`,
+    desktop: `(1024px <= width < 1536px)`,
+    wide: `(1920 <= width)`,
+  },
+  theme: {
+    colors: {
+      ...grayDark,
+      ...violetDark,
+      ...slateDark,
+      ...tomatoDark,
+      ...indigoDark,
+      ...grassDark,
+      ...sageDark,
+      bg: grayDark.gray1,
+    },
+    shadows: {
+      indigo: `${indigoDark.indigo6} 0 4px 16px`,
+      grass: `${grassDark.grass6} 0 4px 16px`,
+    },
+  },
+});
